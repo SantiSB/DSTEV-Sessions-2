9eaa13a611409b6d123668b8847488f455930c49
SantiSB <a.santiago.salas.b@gmail.com>
2024-06-11 15:29:08 -0500

    logos

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,31 +3,46 @@ export const preRegisterNotAvalible = true;
 
 export const links = [
   // { href: "https://docs.google.com/forms/d/e/1FAIpQLScsXFUK5TeHC5pSBSAT_VI13Yiz5S2BqhfSDpVy_grsz2iwdA/viewform?pli=1", text: "Pre-Registro", disabled: preRegisterNotAvalible},
-  { phoneNumber: "573187866592", message: "Hola ! Quiero mi ticket para DSTEV Sessions #2", text: "Tickets", disabled: ticketsNotAvailable },
+  {
+    phoneNumber: "573187866592",
+    message: "Hola ! Quiero mi ticket para DSTEV Sessions #2",
+    text: "Tickets",
+    disabled: ticketsNotAvailable,
+  },
   // { phoneNumber: "573187866592", text: "Contact", disabled: false },
 ];
-
-
 
 export const SPONSORS = [
   {
     id: 1,
+    nombre: "1537",
+    url: "https://www.instagram.com/1537cs/",
+    logo: "/icons/sponsors/1537.png",
+  },
+  {
+    id: 2,
+    nombre: "360cs",
+    url: "https://www.instagram.com/360cs/",
+    logo: "/icons/sponsors/360.png",
+  },
+  {
+    id: 3,
+    nombre: "Jagermeister",
+    url: "https://www.instagram.com/jagermeister_co/",
+    logo: "/icons/sponsors/Jagermeister.png",
+  },
+  {
+    id: 4,
+    nombre: "Selector",
+    url: "https://www.instagram.com/selectormusic_/",
+    logo: "/icons/sponsors/Selector.png",
+  },
+  {
+    id: 5,
     nombre: "Arkhes",
     url: "https://www.instagram.com/__arkhes/",
-    logo: "/icons/MainIcon.png",
+    logo: "/icons/sponsors/Arkhes.png",
   },
-  // {
-  //   id: 2,
-  //   nombre: "Sponsor Dos",
-  //   url: "https://www.sponsordos.com",
-  //   logo: "/icons/Sponsor.png",
-  // },
-  // {
-  //   id: 3,
-  //   nombre: "Sponsor Tres",
-  //   url: "https://www.sponsortres.com",
-  //   logo: "/icons/Sponsor.png",
-  // },
 ];
 
 export const ARTISTS = [
@@ -68,4 +83,3 @@ export const ARTISTS = [
     logo: "/logos/logo6.png",
   },
 ];
-
